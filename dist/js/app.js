d021098d4f3f837db393c8f278776caddf492294
Dima <dima@gmail.com>
2020-11-28 17:41:31 +0200

    Finished nav and added welcome section

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,31 @@
-const menuToggler = document.querySelector('.mobile-menu-toggler');
-const menu = document.querySelector('.nav-list');
+// Select element
+function selectElement(path) {
+    return document.querySelector(path);
+}
+
+
+
+// Open and close mobile menu
+const nav = selectElement('.nav');
+const menuToggler = selectElement('.mobile-menu-toggler');
+const menu = selectElement('.nav-list');
 menuToggler.addEventListener('click', function() {
+    nav.classList.toggle('open');
     menuToggler.classList.toggle('open');
     menu.classList.toggle('open');
+});
+
+// Change Nav style on scroll
+let scrollpos = window.scrollY;
+const nav_height = nav.offsetHeight;
+
+const add_class_on_scroll = () => nav.classList.add('nav-scrolled');
+const remove_class_on_scroll = () => nav.classList.remove("nav-scrolled");
+
+window.addEventListener('scroll', function() { 
+    scrollpos = window.scrollY;
+
+    if (scrollpos >= (nav_height * 2)) { add_class_on_scroll() }
+    else { remove_class_on_scroll() }
+
 });
